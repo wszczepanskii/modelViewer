0a6fde9a99aedcba8a83f0df8bc229b1ab434eec
bvmbo <wiktorsz2001@wp.pl>
2023-07-20 14:37:55 +0200

    removed camera-controls

--- js/modelViewerScript.js
+++ js/modelViewerScript.js
@@ -2,7 +2,6 @@ const arBtn = document.querySelector(".arBtn");
 const arModel = document.querySelector(".arModel");
 
 const toggleAr = () => {
-	arModel.setAttribute("camera-controls", "");
 	arModel.setAttribute("autoplay", "");
 };
 
