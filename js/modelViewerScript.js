de2c8c30a7b8838a08c1be7ee4c0819d326f8e20
bvmbo <wiktorsz2001@wp.pl>
2023-07-20 14:48:58 +0200

    changed scalling

--- js/modelViewerScript.js
+++ js/modelViewerScript.js
@@ -3,6 +3,7 @@ const arModel = document.querySelector(".arModel");
 
 const toggleAr = () => {
 	arModel.setAttribute("autoplay", "");
+	arModel.scale = "0.1 0.1 0.1";
 };
 
 arBtn.addEventListener("click", toggleAr);
